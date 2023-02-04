de5254067faa0abd399d074537599fb77b10d0d2
Inshirah Zainab <inshirahzainab@gmail.com>
2023-02-04 17:59:21 +0530

    Updates

---
+++ precache-manifest.aa923fce5823890dba7d80d65be21806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ac25dc3920e36c21bde",
+    "url": "/textal-text-manipulator-/static/css/main.ce6e1205.chunk.css"
+  },
+  {
+    "revision": "7ac25dc3920e36c21bde",
+    "url": "/textal-text-manipulator-/static/js/main.7ac25dc3.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textal-text-manipulator-/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textal-text-manipulator-/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "e639965686c18cbbe38f",
+    "url": "/textal-text-manipulator-/static/js/runtime~main.e6399656.js"
+  },
+  {
+    "revision": "d3d7c388cb7a6fe33da856d0bb4b909a",
+    "url": "/textal-text-manipulator-/index.html"
+  }
+];
